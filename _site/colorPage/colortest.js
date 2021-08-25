d08f05b78009d0bb4e198987df920a734fa497be
PeytonICF <peyton.stewart@icf.com>
2021-08-25 19:42:48 +0000

    Combined css and js with html and updated colors

--- _site/colorPage/colortest.js
+++ _site/colorPage/colortest.js
@@ -47,7 +47,7 @@ function createItem(color, desc){
 
 const paletteContainer = document.querySelector(".palette");
 
-fetch("https://4000-plum-asp-pvhg4myu.ws-us16.gitpod.io/colorPage/colors.json").then(response => {
+fetch("https://4000-harlequin-aardwolf-5gkd155d.ws-us16.gitpod.io/colorPage/colors.json").then(response => {
 	return response.json();
 }).then(colorList => {
 	for(const{desc, color} of colorList){
